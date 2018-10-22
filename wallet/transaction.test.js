3f9bc5c8d28bfe75247abc483aa328db290405bf
Sandeep <sandeepg.97@gmail.com>
2018-10-22 19:53:39 +0530

    add test - transaction

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -16,4 +16,15 @@ describe('Transaction', () => {
     it('outputs the `amount` added to the recipient', ()=> {
         expect(transaction.outputs.find(output => output.address === recipient).amount).toEqual(amount);
     });
+    
+    describe('Transaction with an amount that exceeds the balance',()=>{
+        beforeEach(()=>{
+            amount = 50000;
+            transaction = Transaction.newTransaction(senderWallet,recipient,amount);
+        });
+
+        it('does not create the transaction',()=>{
+            expect(transaction).toEqual(undefined);
+        });
+    });
 });
