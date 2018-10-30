f4ac4a93938b27e2571b6743dc2ff3b579f12207
Sandeep <sandeepg.97@gmail.com>
2018-10-30 10:59:06 +0530

    add tx verification, test - tx verification

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -19,18 +19,26 @@ class Transaction{
             {amount: senderWallet.balance - amount, address: senderWallet.publicKey },
             {amount, address: recipient}
         ]);
-        Transaction.signTransaction(transaction,senderWallet);
+        transaction.signTransaction(senderWallet);
 
         return transaction;
     }
 
-    static signTransaction(transaction, senderWallet){
+    signTransaction(senderWallet){
         this.input = {
             timestamp: Date.now(),
             amount: senderWallet.balance,
             address: senderWallet.publicKey,
-            signature: senderWallet.sign(ChainUtil.hash(transaction.outputs))
+            signature: senderWallet.sign(ChainUtil.hash(this.outputs))
         }
+    }
+
+    static verifyTransaction(transaction){
+        return ChainUtil.verifySignature(
+            transaction.input.address,
+            transaction.input.signature,
+            ChainUtil.hash(transaction.outputs)
+        )
     }
 }
 
