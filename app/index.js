f6f832ffef2ce05f9d20b57f6e630790be0fd48b
Sandeep <sandeepg.97@gmail.com>
2018-12-08 11:17:00 +0530

    add API - mine transactions

--- app/index.js
+++ app/index.js
@@ -4,12 +4,14 @@ const Blockchain = require('../blockchain');
 const Wallet = require('../wallet');
 const TransactionPool = require('../wallet/transaction-pool');
 const P2pServer = require('./p2p-server');
+const Miner = require('./miner');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 const blockchain = new Blockchain();
 const wallet = new Wallet();
 const transactionPool = new TransactionPool();
 const p2pServer = new P2pServer(blockchain,transactionPool);
+const miner = new Miner(blockchain, transactionPool, wallet, p2pServer);
 
 const app = express()
 app.use(bodyParser.json());
@@ -39,6 +41,12 @@ app.post('/transact',(req,res)=>{
 
 app.get('/public-key',(req,res)=>{
     res.json({publicKey: wallet.publicKey});
+});
+
+app.get('/mine-transactions', (req,res) => {
+    const block = miner.mine();
+    console.log(`New block added: ${block.toString()}`);
+    res.redirect('/blocks');
 });
 
 app.listen(HTTP_PORT,() => console.log(`Listening on port: ${HTTP_PORT}`));
--- app/miner.js
+++ app/miner.js
@@ -1,4 +1,4 @@
-const Wallet = = require('../wallet');
+const Wallet = require('../wallet');
 const Transaction = require('../wallet/transaction');
 
 class Miner{
